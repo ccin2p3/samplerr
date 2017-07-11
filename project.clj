14ecdb535208991503b5458afffb53cee6fafa4a
Fᴀʙɪᴇɴ Wᴇʀɴʟɪ <git@faxmodem.org>
2017-07-11 12:41:25 +0200

    prepare for v0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject samplerr "0.4.0beta1"
+(defproject samplerr "0.4.0"
   :description "riemann plugin to aggregate data in a round-robin fashion to elasticsearch"
   :url "http://github.com/samplerr/samplerr"
   :license {:name "EPL-1.0"
